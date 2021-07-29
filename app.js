8da3c24d3102ab4ae08e76a662c039d38f918bbb
SpyrosTs <73250213+SpyrosTs@users.noreply.github.com>
2021-07-29 15:25:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    getApi();
+})
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            var img = new Image();
+            img.src = result.show.image.medium;
+            document.getElementById('container').appendChild(img);
+        }
+    }
+}
+
+const getApi = async () => {
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config)
+    container.innerHTML = '';
+    makeImages(res.data);
+    form.elements.query.value = '';
+}
